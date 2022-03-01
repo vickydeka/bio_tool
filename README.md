f8edb6925b99c34c891f1fecd73cea733ce03a87
Vicky Deka <vickykumardeka@gmail.com>
2022-03-01 21:07:47 +0530

    completed

---
+++ README.md
@@ -0,0 +1,78 @@
+# Project Bioinformatics
+Biotool is a **Bioinformatics** tools application/ web-portal which enables solution to some major molecular biology techniques for example **Central dogma of molecular biology** and many other created using custom python scripts and using **Django framework** and front end development tools.
+
+
+## About
+
+Django Python based project which uses many different tools like **Python, BioPython, CSS3, HTML5, JavaScript, Heroku, and many more**. 
+
+## Languages and Tools Used:
+
+<img align="left" alt="Visual Studio Code" width="35px"   src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" />
+
+<img align="left" alt="HTML5" width="35px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
+
+<img align="left" alt="CSS3" width="35px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
+
+<img align="left" alt="JavaScript" width="35px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
+
+<img align="left" alt="Python" width="35px" src="https://www.flaticon.com/svg/static/icons/svg/2/2181.svg" />
+
+
+<img align="left" alt="Django" width="35px" src="https://cdn.iconscout.com/icon/free/png-512/django-2-282855.png" />
+
+
+<img align="left" alt="Git" width="35px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png" />
+
+<img align="left" alt="GitHub" width="35px" src="https://www.flaticon.com/svg/static/icons/svg/25/25231.svg" />
+
+<img align="left" alt="Terminal" width="35px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/terminal/terminal.png" />
+
+<br />
+<br />
+
+
+
+## Prerequisite
+
+* Python installed on your machine
+
+* Deployment skills and knowledge using heroku is must for completition of this project.
+
+* Programming knowledge of python, Django, and other front end languages.
+
+* first install a virtual envirmoment through command line using command mentioned below. 
+````
+pip install virtualenvwrapper-win
+````
+
+* create a user using below command
+````
+mkvirtualenv name of your choice
+````
+
+* Running virtual enviroment
+````
+workon name you choosed above
+````
+
+* Install required files using below command
+````
+pip install -r requirements.txt
+````
+
+* Run server using command 
+````
+python manage.py runserver
+````
+
+* The server is now running at [localhost:8000](localhost:8000)
+
+
+## Browser Support
+
+<img src="https://github.com/creativetimofficial/public-assets/blob/master/logos/chrome-logo.png?raw=true" width="35" height="35"> <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/firefox-logo.png" width="35" height="35"> <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/edge-logo.png" width="35" height="35"> <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/safari-logo.png" width="35" height="35"> <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/opera-logo.png" width="35" height="35">
+
+
+
+
---
+++ utility/IUPACData.py
@@ -0,0 +1,122 @@
+
+nucleotides = ["A", "C", "G", "T"]
+
+# Average masses of monophosphate deoxy nucleotides
+unambiguous_dna_weights = {"A": 331.2218, "C": 307.1971, "G": 347.2212, "T": 322.2085}
+
+# Monoisotopic masses of monophospate deoxy nucleotides
+monoisotopic_unambiguous_dna_weights = {
+    "A": 331.06817,
+    "C": 307.056936,
+    "G": 347.063084,
+    "T": 322.056602,
+}
+unambiguous_rna_weights = {"A": 347.2212, "C": 323.1965, "G": 363.2206, "U": 324.1813}
+monoisotopic_unambiguous_rna_weights = {
+    "A": 347.063084,
+    "C": 323.051851,
+    "G": 363.057999,
+    "U": 324.035867,
+}
+
+protein_weights = {
+    "A": 89.0932,
+    "C": 121.1582,
+    "D": 133.1027,
+    "E": 147.1293,
+    "F": 165.1891,
+    "G": 75.0666,
+    "H": 155.1546,
+    "I": 131.1729,
+    "K": 146.1876,
+    "L": 131.1729,
+    "M": 149.2113,
+    "N": 132.1179,
+    "O": 255.3134,
+    "P": 115.1305,
+    "Q": 146.1445,
+    "R": 174.201,
+    "S": 105.0926,
+    "T": 119.1192,
+    "U": 168.0532,
+    "V": 117.1463,
+    "W": 204.2252,
+    "Y": 181.1885,
+}
+
+monoisotopic_protein_weights = {
+    "A": 89.047678,
+    "C": 121.019749,
+    "D": 133.037508,
+    "E": 147.053158,
+    "F": 165.078979,
+    "G": 75.032028,
+    "H": 155.069477,
+    "I": 131.094629,
+    "K": 146.105528,
+    "L": 131.094629,
+    "M": 149.051049,
+    "N": 132.053492,
+    "O": 255.158292,
+    "P": 115.063329,
+    "Q": 146.069142,
+    "R": 174.111676,
+    "S": 105.042593,
+    "T": 119.058243,
+    "U": 168.964203,
+    "V": 117.078979,
+    "W": 204.089878,
+    "Y": 181.073893,
+}
+
+
+RNA_Codons = {
+    # 'M' - START, '_' - STOP
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "UGU": "C", "UGC": "C",
+    "GAU": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "UUU": "F", "UUC": "F",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+    "CAU": "H", "CAC": "H",
+    "AUA": "I", "AUU": "I", "AUC": "I",
+    "AAA": "K", "AAG": "K",
+    "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "AUG": "M",
+    "AAU": "N", "AAC": "N",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "UGG": "W",
+    "UAU": "Y", "UAC": "Y",
+    "UAA": "_", "UAG": "_", "UGA": "_"
+}
+
+DNA_Codons = {
+    # 'M' - START, '_' - STOP
+    "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "TGT": "C", "TGC": "C",
+    "GAT": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "TTT": "F", "TTC": "F",
+    "GGT": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+    "CAT": "H", "CAC": "H",
+    "ATA": "I", "ATT": "I", "ATC": "I",
+    "AAA": "K", "AAG": "K",
+    "TTA": "L", "TTG": "L", "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L",
+    "ATG": "M",
+    "AAT": "N", "AAC": "N",
+    "CCT": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAA": "Q", "CAG": "Q",
+    "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+    "TCT": "S", "TCC": "S", "TCA": "S", "TCG": "S", "AGT": "S", "AGC": "S",
+    "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V",
+    "TGG": "W",
+    "TAT": "Y", "TAC": "Y",
+    "TAA": "_", "TAG": "_", "TGA": "_"
+}
+
